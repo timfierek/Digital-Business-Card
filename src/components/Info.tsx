f3f2d5bf1e103efde0e43eb1cfd5a7bde0e984e9
tim <tfierek357@gmail.com>
2022-10-12 16:36:52 -0400

    rough outline of button links

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -2,13 +2,24 @@ import React from "react";
 import "../styles/Info.css";
 import pfp from "../images/pfp1.jpg";
 
-function Info(){
-    return(
+function Info() {
+    return (
         <div>
             <img src={pfp} alt='' className='profile-pic'></img>
             <h1>Tim Fierek</h1>
             <h2>Backend Engineer</h2>
             <h3>timfierek.com</h3>
+
+            <div className="buttons-container">
+                <a href="mailto: timothyfierek@gmail.com" className="email-button">
+                    <i className="fa-solid fa-envelope"></i>
+                    <p>Email</p>
+                </a>
+                <a href="https://www.linkedin.com/in/tim-fierek/" className="linkedin-button">
+                    <i className="fa-brands fa-linkedin"></i>
+                    <p>LinkedIn</p>
+                </a>
+            </div>
         </div>
     );
 }
